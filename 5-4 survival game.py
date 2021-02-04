f6bd7d99adf331d0725bbc17b8156dbb61d12eb4
fayinac <75463503+fayinac@users.noreply.github.com>
2021-02-03 17:01:28 -0800

    Add files via upload

--- 5-4 survival game.py
+++ 5-4 survival game.py
@@ -12,10 +12,10 @@ canvas.pack()
 title = canvas.create_text(200, 200, text = 'Survive the Spiders', fill = 'black', font = ('Helvetica', 30))
 directions = canvas.create_text(200, 300, text = 'Avoid the spiders at all costs!', fill = 'black', font = ('Helvetica', 30))
 
-#score
-score = 0
-score_display = Label(window, text = "Score: " + str(score))
-score_display.pack()
+#time
+time = 0
+time_display = Label(window, text = "Time: " + str(time))
+time_display.pack()
 
 #level
 level = 1
@@ -35,31 +35,34 @@ spider_image = PhotoImage(file = 'spider.gif')
 
 def make_spider():
    xposition = random.randint(1,400)
-   spider = canvas.create_image(0, xyposition, image = spider_image)
+   spider = canvas.create_image(0, xposition, image = spider_image)
    spider_list.append(spider)
-   window.after(1000, make_spider)
-
+   if level == 1:
+      window.after(1500, make_spider)
+   if level == 2:
+      window.after(700, make_spider)
+   if level == 3:
+      window.after(500, make_spider)
+      
 def move_spider():
    for spider in spider_list:
-      canvas.move(spider, 0, spider_speed)
-      if canvas.coords(spider)[1] > 400:
-         xposition = random.randint(1,400)
-         canvas.coords(spider, xposition, 0, xposition + 30, 30)
+      canvas.move(spider, spider_speed, 0)
    window.after(50, move_spider)
 
-#score update
-def update_score_level():
-   global score, level, spider_speed
-   score = score + 1
-   score_display.config(text = 'Score: ' + str(score))
-   if score > 5 and score <= 10:
+#time update
+def update_time_level():
+   global time, level, spider_speed
+   time = time + 1
+   time_display.config(text = 'Time: ' + str(time))
+   if time > 15 and time <= 30:
       spider_speed = spider_speed + 1
       level = 2
       level_display.config(text = "Level: " + str(level))
-   elif score > 10:
+   elif time > 30:
       spider_speed = spider_speed + 1
       level = 3
-      level_display.config(text = "level: " + str(level))
+      level_display.config(text = "Level: " + str(level))
+   window.after(1000, update_time_level)
 
 def end_game_over():
    window.destroy()
@@ -76,7 +79,7 @@ def collision(item1, item2, distance):
    return overlap
 
 def check_hits():
-   for candy in spider_list:
+   for spider in spider_list:
       if collision(mychar, spider, 30):
          game_over = canvas.create_text(200, 200, text = 'Game Over', fill = 'red', font = ('Helvetica', 30))
          window.after(2000, end_game_over)
@@ -104,12 +107,12 @@ def end_input(event):
 def move_character():
    if move_direction == "Right" and canvas.coords(mychar)[0] < 400:
       canvas.move(mychar, 10,0)
-   elif move_direction == "Left" and canvas.coords(mychar)[0] < 0:
+   elif move_direction == "Left" and canvas.coords(mychar)[0] > 0:
       canvas.move(mychar, -10,0)
-   elif move_direction == "Up" and canvas.coords(mychar)[1] < 0:
+   elif move_direction == "Up" and canvas.coords(mychar)[1] > 0:
       canvas.move(mychar, 0,-10)
    elif move_direction == "Down" and canvas.coords(mychar)[1] < 400:
-      canvas.move(mychar, 0,-10)
+      canvas.move(mychar, 0,10)
    window.after(16, move_character)
 
 canvas.bind_all('<KeyPress>', check_input)
@@ -118,25 +121,9 @@ canvas.bind_all('<KeyRelease>', end_input)
 #start game
 window.after(1000, end_title)
 window.after(1000, make_spider)
+window.after(1000, update_time_level)
 window.after(1000, move_spider)
 window.after(1000, check_hits)
 window.after(1000, move_character)
 
 window.mainloop()
-
-
-
-
-
-
-
-
-
-
-
-
-
-   
-
-
-
